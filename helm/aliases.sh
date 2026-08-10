a270b84638bc922f893aefaae45f6f86aacbfb53
Tong Nguyen Quoc Anh <quocanh010104@gmail.com>
2026-08-10 11:24:02 +0700

    Update aliases.sh

--- helm/aliases.sh
+++ helm/aliases.sh
@@ -1,49 +1,34 @@
 #!/usr/bin/env bash
 
-# =========================
-# Helm Aliases - Productivity Boost
-# =========================
-
 # Core Helm
 alias h='helm'
 alias hi='helm install'
 alias hu='helm upgrade'
 alias hui='helm upgrade --install'
 alias hr='helm rollback'
-alias hs='helm search repo'
-alias hsr='helm search repo'
-alias hse='helm search repo --regexp'
+alias hse='helm search'
+alias hser='helm search repo'
 
 # Charts
 alias hct='helm create'
 alias hlt='helm lint'
 alias hpl='helm pull'
-alias hup='helm repo update'
 
 # Releases
-alias hls='helm list'
-alias hlsa='helm list --all-namespaces'
-alias hst='helm status'
-alias hgt='helm get'
+alias hl='helm list'
+alias hla='helm list --all-namespaces'
+alias hs='helm status'
+alias hg='helm get'
 alias hgv='helm get values'
 alias hga='helm get all'
 
 # Debug / inspect
 alias htpl='helm template'
-alias hval='helm show values'
-alias hsch='helm show chart'
-alias hschr='helm show readme'
-
-# Repo management
-alias hrepo='helm repo'
-alias hrepoa='helm repo add'
-alias hrepou='helm repo update'
-alias hrepol='helm repo list'
+alias hsv='helm show values'
+alias hsc='helm show chart'
 
 # Uninstall
 alias hd='helm uninstall'
 
 # Quick workflow shortcuts
 alias hh='helm history'
-alias hdiff='helm diff upgrade'   # requires helm-diff plugin
-alias hdeps='helm dependency update'
