6196107cc11ed489e08c0ad4a9399b80b971fa0f
Tong Nguyen Quoc Anh <quocanh010104@gmail.com>
2026-08-10 11:07:10 +0700

    Update aliases.sh

--- kubectl/aliases.sh
+++ kubectl/aliases.sh
@@ -1,39 +1,23 @@
 #!/usr/bin/env bash
 
-# =========================
-# Kubectl Aliases - Productivity Boost
-# =========================
-
 # Core
 alias k='kubectl'
 alias kg='kubectl get'
 alias kd='kubectl describe'
 alias kl='kubectl logs'
 alias ke='kubectl edit'
-alias kap='kubectl apply -f'
+alias ka='kubectl apply'
 alias kdel='kubectl delete'
-
-# Context / cluster
-alias kctx='kubectl config current-context'
-alias kctxs='kubectl config get-contexts'
-alias kuse='kubectl config use-context'
-
-# Namespace
-alias kns='kubectl config set-context --current --namespace'
-alias kgns='kubectl get namespaces'
+# kubectl exec [OPTIONS] POD -- COMMAND [ARGS...]
+alias kexec='kubectl exec'
 
 # Pods
 alias kgp='kubectl get pods'
-alias kgpw='kubectl get pods -w'
 alias kdp='kubectl describe pod'
-alias klog='kubectl logs -f'
-alias kexec='kubectl exec -it'
 
 # Deployments
 alias kgd='kubectl get deployments'
 alias kdd='kubectl describe deployment'
-alias krs='kubectl rollout status'
-alias kru='kubectl rollout undo'
 
 # Services
 alias kgs='kubectl get svc'
@@ -46,10 +30,6 @@ alias kdn='kubectl describe node'
 # Events 
 alias kge='kubectl get events --sort-by=.metadata.creationTimestamp'
 
-# Quick debugging combo
-alias ktop='kubectl top pod'
-alias ktopn='kubectl top node'
-
 # YAML output 
 alias kgy='kubectl get -o yaml'
-alias kgo='kubectl get -o wide'
+alias kgw='kubectl get -o wide'
